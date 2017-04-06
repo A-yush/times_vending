c7b3a5a5bebb3b432ccb5fcee470d7b8ebc84778
Ayush Sharma <a.yush11@yahoo.com>
2017-04-06 13:45:59 +0530

    Add files via upload

---
+++ php/nav.php
@@ -0,0 +1,45 @@
+
+<!-- Navigation -->
+  <nav class="navbar navbar-fixed-top navi navbar-inverse" role="navigation">
+       <div class="container">
+           <!-- Brand and toggle get grouped for better mobile display -->
+           <div class="navbar-header">
+               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+               </button>
+               <a class="navbar-brand" href="index.php"><img src="images/times121.png" alt="Times Vending" style="height:75px;width: 140px;position: fixed;left:65px;top:5px;" ></a>
+            </div>
+            
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">  
+                <li>
+                        <a href="index.php" class="hvr-hang ul_a"  style="font-family:'NewCenturySchlbk-Bold';font-weight: bold; color:white;">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.php" class="hvr-hang ul_a" style="font-family:'NewCenturySchlbk-Bold';font-weight: bold;color:white;">About us</a>
+                    </li>
+                    <li>
+                        <a href="products.php" class="hvr-hang ul_a" style="font-family:'NewCenturySchlbk-Bold';font-weight: bold;color:white;">Products</a>
+                    </li> 
+                     <li>
+                        <a href="lc.php" class="hvr-hang ul_a" style="font-family:'NewCenturySchlbk-Bold';font-weight: bold;color:white;">Labour Compliances</a>
+                    </li>
+                    <li>
+                        <a href="contact.php" class="hvr-hang ul_a" style="font-family:'NewCenturySchlbk-Bold';font-weight: bold;color:white;">Contact us</a>
+                    </li>
+                   
+                   
+                   
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    
+  
+  
